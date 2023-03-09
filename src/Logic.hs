f2d11b59d3053c0a7fed4db5cb550654024e9598
Max Lantas <mnxndev@outlook.com>
2023-03-09 11:00:12 -0800

    implement Logic.liberties

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,10 +1,12 @@
 module Logic (
     neighbors,
+    liberties,
 ) where
 
 import Board (Board, Position)
 import Board qualified
 
+import Control.Monad (filterM)
 import Data.Maybe (mapMaybe)
 
 neighbors :: Board -> Position -> [Position]
@@ -17,3 +19,9 @@ neighbors b pos =
             , (row + 1, column)
             , (row, column - 1)
             ]
+
+liberties :: Board -> Position -> IO [Position]
+liberties b pos = filterM isEmpty (neighbors b pos)
+  where
+    isEmpty :: Position -> IO Bool
+    isEmpty = fmap (== Board.Empty) . Board.get b
--- test/LogicSpec.hs
+++ test/LogicSpec.hs
@@ -43,3 +43,30 @@ spec = do
                 neighbors = Logic.neighbors b pos
             pieces <- mapM (Board.get b) neighbors
             pieces `shouldBe` [black, Empty, white, black]
+
+    describe "Logic.liberties" $ do
+        let boardIO =
+                Board.fromList
+                    4
+                    [ [black, black, black, Empty]
+                    , [Empty, black, Empty, black]
+                    , [white, Empty, white, Empty]
+                    , [Empty, white, Empty, white]
+                    ]
+
+        it "returns 0 positions when surrounded" $ do
+            b <- boardIO
+            let pos = fromJust $ Board.position b (1, 0)
+            Logic.liberties b pos >>= (`shouldBe` [])
+
+        it "returns between 1 and 4 positions when not completely surrounded" $ do
+            b <- boardIO
+            let positionList = fmap (fromJust . Board.position b)
+                p00 = fromJust $ Board.position b (0, 0)
+                p02 = fromJust $ Board.position b (0, 2)
+                p20 = fromJust $ Board.position b (2, 0)
+                p22 = fromJust $ Board.position b (2, 2)
+            Logic.liberties b p00 >>= (`shouldBe` positionList [(1, 0)])
+            Logic.liberties b p02 >>= (`shouldBe` positionList [(0, 3), (1, 2)])
+            Logic.liberties b p20 >>= (`shouldBe` positionList [(1, 0), (2, 1), (3, 0)])
+            Logic.liberties b p22 >>= (`shouldBe` positionList [(1, 2), (2, 3), (3, 2), (2, 1)])
