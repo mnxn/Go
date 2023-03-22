11641a3f4305465e722f3d8ecf2b5c5a708855b3
Max Lantas <mnxndev@outlook.com>
2023-03-22 00:33:55 -0700

    use MonadIO in Logic module

--- src/Logic.hs
+++ src/Logic.hs
@@ -7,21 +7,22 @@ module Logic (
 ) where
 
 import Control.Monad (filterM)
+import Control.Monad.Except (ExceptT, MonadError (throwError))
+import Control.Monad.IO.Class (MonadIO)
+import Data.Foldable (foldrM)
 import Data.Maybe (mapMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
 
 import Board (Board, Position)
 import Board qualified
-import Control.Monad.Except (ExceptT, MonadError (throwError), MonadTrans (lift))
-import Data.Foldable (foldrM)
 
 data LogicError
     = PositionTaken
     | SelfCapture
     deriving (Show, Eq)
 
-isPiece :: Board -> Board.Piece -> Position -> IO Bool
+isPiece :: MonadIO io => Board -> Board.Piece -> Position -> io Bool
 isPiece b piece = fmap (== piece) . Board.get b
 
 neighbors :: Board -> Position -> [Position]
@@ -35,12 +36,12 @@ neighbors b pos =
             , (row, column - 1)
             ]
 
-group :: Board -> Position -> IO (Set Position)
+group :: MonadIO io => Board -> Position -> io (Set Position)
 group b start = do
     target <- Board.get b start
     Set.insert start <$> group' target Set.empty start
   where
-    group' :: Board.Piece -> Set Position -> Position -> IO (Set Position)
+    group' :: MonadIO io => Board.Piece -> Set Position -> Position -> io (Set Position)
     group' target set pos
         | Set.member pos set = return set
         | otherwise = do
@@ -49,34 +50,34 @@ group b start = do
                 then mconcat <$> mapM (group' target (Set.insert pos set)) (neighbors b pos)
                 else return Set.empty
 
-liberties :: Board -> Position -> IO (Set Position)
+liberties :: MonadIO io => Board -> Position -> io (Set Position)
 liberties b start = do
     g <- group b start
     foldrM liberties' Set.empty g
   where
-    liberties' :: Position -> Set Position -> IO (Set Position)
+    liberties' :: MonadIO io => Position -> Set Position -> io (Set Position)
     liberties' pos acc = mappend acc . Set.fromList <$> filterM (isPiece b Board.Empty) (neighbors b pos)
 
-play :: Board -> Position -> Board.Player -> ExceptT LogicError IO (Set Position)
+play :: MonadIO io => Board -> Position -> Board.Player -> ExceptT LogicError io (Set Position)
 play b pos player = do
     currentPiece <- Board.get b pos
     case currentPiece of
         Board.Piece _ -> throwError PositionTaken
         Board.Empty -> do
             Board.set b pos (Board.Piece player)
-            ownLiberties <- lift $ liberties b pos
-            oppositeNeighbors <- lift $ filterM isOppositePiece (neighbors b pos)
-            oppositeCaptures <- lift $ captures oppositeNeighbors
+            ownLiberties <- liberties b pos
+            oppositeNeighbors <- filterM isOppositePiece (neighbors b pos)
+            oppositeCaptures <- captures oppositeNeighbors
             if Set.null ownLiberties && Set.null oppositeCaptures
                 then do
                     Board.remove b pos
                     throwError SelfCapture
                 else return oppositeCaptures
   where
-    isOppositePiece :: Position -> IO Bool
+    isOppositePiece :: MonadIO io => Position -> io Bool
     isOppositePiece = isPiece b (Board.Piece $ Board.opposite player)
 
-    captures :: [Position] -> IO (Set Position)
+    captures :: MonadIO io => [Position] -> io (Set Position)
     captures [] = return Set.empty
     captures (p : ps) = do
         ls <- liberties b p
