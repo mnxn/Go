2e82be4762ce4f2b1cab5e643f3b3218d489ee83
Max Lantas <mnxndev@outlook.com>
2023-03-22 09:31:03 -0700

    command line arguments for display mode

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,28 @@
 module Main (main) where
 
 import Main.Utf8 (withUtf8)
+import System.Environment
 import System.IO.CodePage (withCP65001)
 
 import Game qualified
 
 main :: IO ()
-main = withUtf8 $ withCP65001 $ Game.runGame True
+main = withUtf8 $ withCP65001 $ do
+    args <- getArgs
+    case args of
+        [] -> Game.runGame True
+        ["--ansi"] -> Game.runGame True
+        ["--ascii"] -> Game.runGame False
+        _ -> putStrLn helpMessage
+
+helpMessage :: String
+helpMessage =
+    unlines
+        [ "Usage:"
+        , "  --ascii"
+        , "        use ascii output"
+        , "  --ansi"
+        , "        use ansi output"
+        , "  --help"
+        , "        show this help message"
+        ]
