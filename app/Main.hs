90578efe55fb3485f53fa16cdb5154d935ea4c4e
Max Lantas <mnxndev@outlook.com>
2023-03-22 09:03:07 -0700

    fix unicode printing on Windows

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main (main) where
 
+import Main.Utf8 (withUtf8)
+import System.IO.CodePage (withCP65001)
+
 import Game qualified
 
 main :: IO ()
-main = Game.makeState 7 >>= Game.runGame
+main = withUtf8 $ withCP65001 $ Game.makeState 7 >>= Game.runGame
